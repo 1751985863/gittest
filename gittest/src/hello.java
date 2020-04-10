7274036b2c5267ed1f17e9a957c3bc0db5c03c01
yangnanfeng <1751985863@qq.com>
2020-04-10 21:36:20 +0800

    第二次修改

---
+++ gittest/src/hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hhhhhhh");
+        System.out.println("ddddddd");
+    }
+    public void show(){
+        System.out.println();
+    }
+}
