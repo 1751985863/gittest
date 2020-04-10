478f4b86c1006bc5bfc6c13b20b66ba4c0b749eb
yangnanfeng <1751985863@qq.com>
2020-04-10 21:43:26 +0800

    第三次修改

--- gittest/src/hello.java
+++ gittest/src/hello.java
@@ -8,4 +8,10 @@ public class hello {
     public void show(){
         System.out.println();
     }
+    public void show1(){
+        System.out.println("hhheee");
+    }
+    public void show2(){
+        System.out.println("dfdfdfss");
+    }
 }
